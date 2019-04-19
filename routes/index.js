d844e9c884fc3ef74c9a3a6fbb664495ad9351fb
tw0winju <fakeboy62@gmail.com>
2019-04-19 11:54:50 +0900

    요일 수정

--- routes/index.js
+++ routes/index.js
@@ -13,14 +13,17 @@ app.get('/', (req, res) => {
     const data = JSON.parse(body);
     const lunch = [];
     const date = [];
+    const day = ["월","화","수","목","금","토","일"];
     for(let i = 0; i < data.menu.length; i++) {
-      lunch.push(data.menu[i].lunch);
+      lunch.push(data.menu[i].lunch); 
       date.push(data.menu[i].date);
+      
     }
     res.render('lunch.ejs', {
       lunch: lunch,
       date: date,
-      menu: data.menu
+      menu: data.menu,
+      day: day
     });
   });
   });
